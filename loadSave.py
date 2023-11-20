86310d6a44e2bdffab1998853924230ad2cd8fbe
Sowmya R <soumya80@gmail.com>
2023-11-20 18:02:53 -0500

    Update loadSave.py

--- loadSave.py
+++ loadSave.py
@@ -1,3 +1,7 @@
+'''
+loads the saved pytorch model from the given location. 
+For the file mentioned in this code, transfer_learning.py need to be executed to generate the model and save in the location. 
+'''
 import torch
 model = torch.load('model/transfer_learning.pth')
 print(model.state_dict())
