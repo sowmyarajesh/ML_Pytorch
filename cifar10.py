e7a63ba0abed21d5dce00c2d96342b75787dd9ec
Sowmya R <soumya80@gmail.com>
2023-11-20 09:45:42 -0500

    Update cifar10.py

--- cifar10.py
+++ cifar10.py
@@ -1,20 +1,136 @@
+'''
+Will use CNN model to classify the CIFAR data
+This dataset is available in the pytorch. 
+Reference: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+
+'''
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# Hyper-parameters 
+num_epochs = 5
+batch_size = 4
+learning_rate = 0.001
+
+# Loading Dataset
+# ================
+# #dataset has PILImage images of range [0, 1]. 
+# #We transform them to Tensors of normalized range [-1, 1]
 tranform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
 )
 
 batch_size = 4
-
+### When in GPU system, we can try to set num_workers>0. In windows, with CPU, it can get stuck on freeze_support() when num_workers>0
 trainset = torchvision.datasets.CIFAR10(root='./data',train=True, download=True,transform=tranform)
-trainloader = torch.utils.data.DataLoader(trainset,batch_size=batch_size, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset,batch_size=batch_size, shuffle=True, num_workers=0)
 
 testset = torchvision.datasets.CIFAR10(root='./data',train=True, download=True,transform=tranform)
-testloader = torch.utils.data.DataLoader(trainset,batch_size=batch_size, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(trainset,batch_size=batch_size, shuffle=True, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
+# #functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# Define  Model
+# ==============
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120) # fully connected layer
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self,x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+CNN = ConvNet().to(device)
+
+#  Define a Loss function and optimizer
+# =======================================
+criterion = nn.CrossEntropyLoss() # Since we have multiclass classification 
+optimizer = torch.optim.SGD(CNN.parameters(), lr=learning_rate, momentum=0.9)
+
+
+# Training
+# ============
+n_total_steps = len(trainloader)
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, (images, labels) in enumerate(trainloader):
+        # origin shape: [4, 3, 32, 32] = 4, 3, 1024
+        # input_layer: 3 input channels, 6 output channels, 5 kernel size
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = CNN(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+PATH = './cnn.pth'
+torch.save(CNN.state_dict(), PATH)
+
+# Testing and evaluation
+# =======================
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+    for images, labels in testloader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = CNN(images)
+        # max returns (value ,index)
+        _, predicted = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+        
+        for i in range(batch_size):
+            label = labels[i]
+            pred = predicted[i]
+            if (label == pred):
+                n_class_correct[label] += 1
+            n_class_samples[label] += 1
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network: {acc} %')
+
+    for i in range(10):
+        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        print(f'Accuracy of {classes[i]}: {acc} %')
